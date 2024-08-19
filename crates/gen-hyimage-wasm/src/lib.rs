a3693c6aa2e267553e403997b0112b3cf1b2da56
HirokiYasui <hiromaily2@gmail.com>
2024-08-19 16:40:22 +0900

    add rgb parameters

--- crates/gen-hyimage-wasm/src/lib.rs
+++ crates/gen-hyimage-wasm/src/lib.rs
@@ -5,16 +5,19 @@ use wasm_bindgen::prelude::*;
 // Export a `generate_image` function from Rust to JavaScript,
 // that generates image.
 #[wasm_bindgen]
-pub fn generate_image() -> Vec<u8> {
+pub fn generate_image(r: u8, g: u8, b: u8) -> Vec<u8> {
     // Create a 100x100 pixel image with a solid color
     let mut img = RgbImage::new(100, 100);
-    for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let color = if (x + y) % 2 == 0 {
-            Rgb([255, 0, 0])
-        } else {
-            Rgb([0, 255, 0])
-        };
-        *pixel = color;
+    // for (x, y, pixel) in img.enumerate_pixels_mut() {
+    //     let color = if (x + y) % 2 == 0 {
+    //         Rgb([255, 0, 0])
+    //     } else {
+    //         Rgb([0, 255, 0])
+    //     };
+    //     *pixel = color;
+    // }
+    for (_, _, pixel) in img.enumerate_pixels_mut() {
+        *pixel = Rgb([r, g, b]);
     }
 
     // Convert the image to PNG format
